b55556b3105b0173fc3dc57454f3f0a8c6ef6acc
Nicole <76132106+nicolecomputer@users.noreply.github.com>
2024-04-24 11:03:26 -0600

    first implementation

--- aoc-24-01/aoc/day01.go
+++ aoc-24-01/aoc/day01.go
@@ -1,5 +1,23 @@
 package aoc
 
+import "strconv"
+
 func CalibrationValue(line string) int {
-	return 0
+	var firstNum int64 = -1
+	var lastNum int64 = -1
+
+	for i := 0; i < len(line); i++ {
+		num, err := strconv.ParseInt(string(line[i]), 10, 64)
+		if err != nil {
+			continue
+		}
+
+		if firstNum == -1 {
+			firstNum = num
+		} else {
+			lastNum = num
+		}
+	}
+
+	return int(firstNum)*10 + int(lastNum)
 }
