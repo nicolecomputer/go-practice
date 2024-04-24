10b2029a591356b9eb1ba4c723473a833aa244f4
Nicole <76132106+nicolecomputer@users.noreply.github.com>
2024-04-24 11:04:35 -0600

    refactor test runner

--- aoc-24-01/aoc/day01_test.go
+++ aoc-24-01/aoc/day01_test.go
@@ -2,13 +2,16 @@ package aoc
 
 import "testing"
 
+func assertEqual(t *testing.T, expected int, got int) {
+	if expected != got {
+		t.Errorf("expected %d got %d", expected, got)
+	}
+}
+
 func TestCalibrationValue(t *testing.T) {
 	t.Run("1abc2", func(t *testing.T) {
 		expected := 12
 		got := CalibrationValue("1abc2")
-
-		if expected != got {
-			t.Errorf("expected %d got %d", expected, got)
-		}
+		assertEqual(t, expected, got)
 	})
 }
